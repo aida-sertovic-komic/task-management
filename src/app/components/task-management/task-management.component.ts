b697e53bd76a80f0de75e838815cedecada276c7
Aida Šertović Komić <sertovicaida@gmail.com>
2022-11-15 19:16:15 +0100

    create random color for task

--- src/app/components/task-management/task-management.component.ts
+++ src/app/components/task-management/task-management.component.ts
@@ -9,18 +9,32 @@ import { Task } from '../models/task.model';
 })
 export class TaskManagementComponent implements OnInit {
   public task!: Task;
+  public tasks: Task[] = [];
   public taskForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
     this.taskForm = new FormGroup({
       title: new FormControl(''),
-      description: new FormControl('')
+      description: new FormControl(''),
+      color: new FormControl('')
     })
   }
 
   public createTask(): void {
-    console.log(this.taskForm.value)
+    this.taskForm.get('color')?.patchValue(this.createRandomColor());
+    this.tasks.push(this.taskForm.value);
+    localStorage.setItem('Tasks', JSON.stringify(this.tasks)) //store in localStorage
+    // localStorage.clear();
+
+  }
+
+  public createRandomColor(): string {
+    const maxColorValue = 0xFFFFFF;
+    let randomNumber = Math.random() * maxColorValue; //create random number
+    randomNumber = Math.floor(randomNumber); //convert float number to an integer
+    const randomHexColor = '#' + randomNumber.toString(16); //convert integer to hexadecimal string
+    return randomHexColor;
   }
 
 }
