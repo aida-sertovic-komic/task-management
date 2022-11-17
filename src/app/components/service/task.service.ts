6b3f8258ee06b029bff35091e4b40c5ccf366077
Aida Šertović Komić <sertovicaida@gmail.com>
2022-11-17 22:34:13 +0100

    add service for changed tasks

---
+++ src/app/components/service/task.service.ts
@@ -0,0 +1,13 @@
+import { Subject } from "rxjs";
+
+export class TaskService {
+    tasksChanged = new Subject<Task[]>();
+    tasks: Task[] = [];
+
+    addTask(task: Task): void {
+        this.tasks.push(task);
+        this.tasksChanged.next(this.tasks);
+        localStorage.setItem('Tasks', JSON.stringify(this.tasks)) //store in localStorage
+
+    }
+}
