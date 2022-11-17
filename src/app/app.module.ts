6bf46960bad8e334281c6f364e78248580492bd8
Aida Šertović Komić <sertovicaida@gmail.com>
2022-11-17 20:08:25 +0100

    Implement table for previewing tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TaskManagementComponent } from './components/task-management/task-management.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
- 
+import { TaskPreviewComponent } from './components/task-management/task-preview/task-preview.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TaskManagementComponent,
-    FooterComponent
+    FooterComponent,
+    TaskPreviewComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,9 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTableModule, MatPaginatorModule
+
 
   ],
   providers: [],
